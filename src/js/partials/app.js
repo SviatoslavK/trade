542022914a62256c5227baf5fa4ed40181c74da3
Slava <kolodaslav@gmail.com>
2017-09-15 20:40:15 +0300

    add pages. scale 75%

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,28 +1,30 @@
 
 // Show/hide filters
-let filterBlock = document.querySelector('.filter__dropdown'),
-    showBtn = document.querySelector('.inputfield__show'),
-    showArrow = showBtn.querySelector('.inputfield__arrow'),
-    showText = showBtn.querySelector('.show__text');
+let filterBlock = document.querySelector('.filter__dropdown');
+if (filterBlock) {
+    let showBtn = document.querySelector('.inputfield__show');
+    let showArrow = showBtn.querySelector('.inputfield__arrow');
+    let showText = showBtn.querySelector('.show__text');
 
-filterBlock.style.height = '0px';
-filterBlock.style.overflow = 'hidden';
-filterBlock.style.transition = 'height 1s';
+    filterBlock.style.height = '0px';
+    filterBlock.style.overflow = 'hidden';
+    filterBlock.style.transition = 'height 1s';
 
-showBtn.addEventListener('click', () => {
-    if (!showBtn.hasAttribute('show')) {
-        showBtn.setAttribute('show', '');
-        showText.innerHTML = 'Згорнути розширений пошук'
-        filterBlock.style.height = 'auto';
-        showArrow.style.transform = 'rotate(0deg)'
-    } else {
-        showBtn.removeAttribute('show');
-        showText.innerHTML = 'Розгорнути розширений пошук'
-        filterBlock.style.height = '0px';
-        showArrow.style.transform = 'rotate(180deg)'
-    }
+    showBtn.addEventListener('click', () => {
+        if (!showBtn.hasAttribute('show')) {
+            showBtn.setAttribute('show', '');
+            showText.innerHTML = 'Згорнути розширений пошук'
+            filterBlock.style.height = 'auto';
+            showArrow.style.transform = 'rotate(0deg)'
+        } else {
+            showBtn.removeAttribute('show');
+            showText.innerHTML = 'Розгорнути розширений пошук'
+            filterBlock.style.height = '0px';
+            showArrow.style.transform = 'rotate(180deg)'
+        }
 
-})
+    })
+}
 
 
 // Scroll to top
@@ -85,11 +87,11 @@ chooseBtn.addEventListener('click', () => {
 let allLang = [...document.querySelectorAll('.lang__block')]
 allLang.map(btn => {
     btn.addEventListener('click', e => {
-        if(e.target.getAttribute('class').includes('lang__flag') || e.target.getAttribute('class').includes('lang__language')) {
+        if (e.target.getAttribute('class').includes('lang__flag') || e.target.getAttribute('class').includes('lang__language')) {
             checkElementOnce(chooseBlock, 'div', e.target.parentElement);
-                    chooseBtn.removeAttribute('show');
-                    chooseBlock.style.height = '20px';
-                    chooseBlock.style.marginTop = '0';
+            chooseBtn.removeAttribute('show');
+            chooseBlock.style.height = '20px';
+            chooseBlock.style.marginTop = '0';
         }
     })
 })
